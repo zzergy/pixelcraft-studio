cd7223ea8d356f93af4f9d5a2be7af32f03d1b46
zergy <xxdarkbluedevilxx@gmail.com>
2023-10-29 17:19:28 +0200

    Replace the hardcodded routes with variables

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,17 @@ import './font.css'
 import 'semantic-ui-css/semantic.min.css'
 import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Homepage from './Pages/Homepage/Homepage';
+import { homepage } from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to='/homepage' />,
+    element: <Navigate to={homepage} />,
   },
   {
-    path: '/homepage',
+    path: homepage,
     element: <App />
   }
 ]);
