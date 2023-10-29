0734dd8d03eb2d2d804815d6531faa38aa4c25eb
zergy <xxdarkbluedevilxx@gmail.com>
2023-10-29 21:11:01 +0200

    Migrate PixelButton to a separate component

--- src/Shared/ErrorPage/ErrorPage.tsx
+++ src/Shared/ErrorPage/ErrorPage.tsx
@@ -1,8 +1,17 @@
 import { faFaceFrown } from "@fortawesome/free-regular-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import styles from './ErrorPage.module.scss'
+import PixelButton from "../PixelButton/PixelButton"
+import { homepage } from "../../routes"
+import { useNavigate } from "react-router-dom"
 
 const ErrorPage = () => {
+    const navigate = useNavigate();
+
+    const handleRedirect = () => {
+        navigate(homepage)
+    }
+
     return (
         <div className={styles.container}>
             <FontAwesomeIcon icon={faFaceFrown} className={styles.sadFaceIcon} size="8x" />
@@ -10,6 +19,7 @@ const ErrorPage = () => {
             <span className={styles.text}>The pixel path you're on doesn't exist!<br />
                 Let's go back to the starting point and explore again.
             </span>
+            <PixelButton label='GO BACK' onClick={handleRedirect} />
         </div>
     )
 }
