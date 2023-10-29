733fafb9146d18b72a9cc55fc644c6e69eac5e8c
zergy <xxdarkbluedevilxx@gmail.com>
2023-10-29 22:30:32 +0200

    Start Development on left Nav

--- src/Pages/CanvasPage/CanvasPage.tsx
+++ src/Pages/CanvasPage/CanvasPage.tsx
@@ -1,5 +1,12 @@
+import Header from "../../Shared/Header/Header"
+import LeftNav from "../LeftNav/LeftNav"
+import styles from './CanvasPage.module.scss'
+
 const CanvasPage = () => {
-    return (<div>asd</div>)
+    return (<div className={styles.container}>
+        <Header />
+        <LeftNav />
+    </div>)
 }
 
 export default CanvasPage
---
+++ src/Pages/LeftNav/LeftNav.tsx
@@ -0,0 +1,11 @@
+import styles from './LeftNav.module.scss'
+
+const LeftNav = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+
+export default LeftNav
