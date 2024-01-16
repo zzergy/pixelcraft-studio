c4cc920207da5ac191b21ebc427c1b4e745e7429
zergy <xxdarkbluedevilxx@gmail.com>
2024-01-16 23:59:14 +0200

    Change homepage layout and design

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom'
 const Homepage = () => {
     const navigate = useNavigate();
 
-    const introText = "WELCOME TO PIXEL PERFECT CANVAS WHERE YOUR CREATIVITY MEETS PIXEL ART EXCELLENCE. CRAFT STUNNING AND DETAILED PIXEL ART WITH EASE. DIVE INTO A WORLD OF ENDLESS POSSIBILITIES AND BRING YOUR IMAGINATION TO LIFE, ONE PIXEL AT A TIME. START CREATING YOUR PIXELATED MASTERPIECE TODAY"
+    const introText = "Welcome to Pixel Perfect Canvas where your creativity meets pixel art excellence. Craft stunning and detailed pixel art with ease. Dive into a world of endless possibilities and bring your imagination to life, one pixel at a time. Start creating your pixelated masterpiece today."
 
     const handleRedirect = () => {
         navigate(canvas)
@@ -16,16 +16,13 @@ const Homepage = () => {
     return (
         <div className={styles.container}>
             <div className={styles.blurContainer}>
-                <h2 className={styles.span}>UNLEASH YOUR INNER ARTIST WITH</h2>
+                <h2 className={styles.title}>UNLEASH YOUR INNER ARTIST WITH</h2>
                 <img src={logo} alt='logo' className={styles.logo} />
                 <span className={styles.span}>
                     {introText}
                 </span>
             </div>
             <PixelButton label='BEGIN YOUR PIXEL QUEST' size='lg' onClick={handleRedirect} />
-            <a href='https://www.markferrari.com/recent-artwork' className={styles.artworkCredits}>
-                ARTWORK BY: MARK FERRARI
-            </a>
         </div>
     )
 }
