8a8a5edad8de09acc3635c94ee8f127575bdf78e
zergy <xxdarkbluedevilxx@gmail.com>
2024-01-31 00:00:03 +0200

    Visual enhancements

--- src/Shared/ErrorPage/ErrorPage.tsx
+++ src/Shared/ErrorPage/ErrorPage.tsx
@@ -15,7 +15,7 @@ const ErrorPage = () => {
     return (
         <div className={styles.container}>
             <FontAwesomeIcon icon={faFaceFrown} className={styles.sadFaceIcon} size="8x" />
-            <h2>Oops..</h2>
+            <h2>Oops...</h2>
             <span className={styles.text}>The pixel path you're on doesn't exist!<br />
                 Let's go back to the starting point and explore again.
             </span>
