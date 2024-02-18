de2baa6bebf49f3d712d543cdd14a027dc8da13a
zergy <xxdarkbluedevilxx@gmail.com>
2024-02-18 20:16:38 +0200

    Fix routing issues with the deployed ver

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Homepage from './Pages/Homepage/Homepage';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom';
 import { canvas, homepage } from './routes';
 import ErrorPage from './Shared/ErrorPage/ErrorPage';
 import CanvasPage from './Pages/CanvasPage/CanvasPage';
@@ -10,7 +10,7 @@ import { customTheme } from './customTheme';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const App = () => {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: homepage,
       element: <Homepage />,
